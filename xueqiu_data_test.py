5d0191aba6bcf8d29f346e56c0515ba1d6d4d114
Gary_Cheung <sirius.ziham@gmail.com>
2016-05-13 11:51:10 +0800

    update headers

--- xueqiu_data_test.py
+++ xueqiu_data_test.py
@@ -6,27 +6,7 @@ import time
 import re
 
 stock_list = [
-    'SZ000543',    #皖能电力
-    'SZ002041',    #登海种业
-    'SH600050',    #中国联通
-    'SZ002215',    #诺普信
-    'SH600789',    #鲁抗医药
-    'SZ300027',    #华谊兄弟
-    'SH600133',    #东湖高新
-    'SZ300074',    #华平股份
-    'SZ002178',    #延华智能
-    'SZ300315',    #掌趣科技
-    'SZ002565',    #上海绿新
-    'SZ000705',    #浙江震元
-    'SH600677',    #航天通信
-    'SZ002658',    #雪迪龙
-    'SZ000563',    #陕国投A
-    'SZ002345',    #潮宏基
-    'SZ002699',    #美盛文化
-    'SH600251',    #冠农股份
-    'SZ000587',    #金洲慈航, 金叶珠宝
-    'SZ002553',    #南方轴承
-    'SZ002240'     #威华股份
+    'SZ000543'    #皖能电力
 ]
 
 headers = {
@@ -66,27 +46,11 @@ def get_stock_amplitude(stock_list):
         print('-------------------------',url)
         web_data = requests.get(url,headers=headers)
         soup = BeautifulSoup(web_data.text,'lxml')
-        #print(soup)
+        print(soup)
         stock_quantity = soup.select('table.topTable > tr:nth-of-type(2) > td:nth-of-type(4)')
         stock_amplitude = soup.select('table.topTable > tr:nth-of-type(5) > td:nth-of-type(1) > span')
         stock_name = soup.select('strong.stockName')
         print(stock_quantity,stock_amplitude,stock_name)
-        for quantity,amplitude,name in zip(stock_quantity,stock_amplitude,stock_name):
-            quantities = re.findall(r'(\w*[0-9]+\.*[0-9]+)\w*',quantity.get_text())
-            amplitudes = re.findall(r'(\w*[0-9]+\.*[0-9]+)\w*',amplitude.get_text())
-            names = name.get_text()
-            print(quantities,'-------------',amplitudes,'-----------------',names)
-            connection = pymysql.connect(**config)
-            try:
-                with connection.cursor() as cursor:
-                    # 执行sql语句，插入记录
-                    sql = 'INSERT INTO stock_data (date, quantity, amplitude, stock_name) VALUES (%s, %s, %s, %s)'
-                    cursor.execute(sql, (present_date, quantities, amplitudes, names))
-                    # 没有设置默认自动提交，需要主动提交，以保存所执行的语句
-                connection.commit()
-            finally:
-                connection.close()
-    time.sleep(1)
 
 # delete_current_data(config)
 get_stock_amplitude(stock_list)
